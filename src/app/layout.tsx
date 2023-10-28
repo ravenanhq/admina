70714ea354490e94f0267e8946ed0853839e67ae
Karthic <karthic.ganesan@ravenantech.com>
2023-10-28 18:48:37 +0530

    Mobile grid ui issue fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 "use client";
+import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
-import * as React from "react";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import { NavbarProvider } from "@/contexts/NavbarContext";
 import Head from "next/head";
 import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const theme = useTheme();
+  const isMobile = useMediaQuery("(max-width:1023px)");
+
   return (
     <html>
       <Head>
@@ -30,25 +33,43 @@ export default function RootLayout({
       </Head>
       <body suppressHydrationWarning={true} className={inter.className}>
         <NavbarProvider>
-          <Box sx={{ display: "flex" }}>
+          {isMobile ? (
+            <Box component="main"
+            sx={{
+              flexGrow: 1,
+              p: 3,
+              marginTop: "30px",
+            }}>
             <CssBaseline />
             <Header />
             <Sidebar />
-            <Box
-              component="main"
-              sx={{
-                flexGrow: 1,
-                p: 3,
-                marginTop:'30px'
-              }}
-            >
               <div style={{ paddingTop: theme.spacing(2) }}>
                 {" "}
                 {/* Use theme spacing for consistent spacing */}
                 {children}
               </div>
-            </Box>
           </Box>
+          ) : (
+            <Box sx={{ display: "flex" }}>
+              <CssBaseline />
+              <Header />
+              <Sidebar />
+              <Box
+                component="main"
+                sx={{
+                  flexGrow: 1,
+                  p: 3,
+                  marginTop: "30px",
+                }}
+              >
+                <div style={{ paddingTop: theme.spacing(2) }}>
+                  {" "}
+                  {/* Use theme spacing for consistent spacing */}
+                  {children}
+                </div>
+              </Box>
+            </Box>
+          )}
         </NavbarProvider>
       </body>
     </html>
