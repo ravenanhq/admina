cd83b970c28b6de299513a728bd30c7774e5fbdc
Karthic <karthic.ganesan@ravenantech.com>
2023-12-01 11:23:38 +0530

    Alert and Tabs UI issue fixed

--- src/app/components/AlertElement/AlertElement.tsx
+++ src/app/components/AlertElement/AlertElement.tsx
@@ -17,33 +17,33 @@ const AlertElements = () => {
         titleTypographyProps={{ fontSize: "16px" }}
       />
       <CardContent>
-        <Box
-          sx={{
-            marginTop: "16px",
-            display: "flex",
-            justifyContent: "space-between",
-            flexWrap: "wrap",
-            gap: "18px",
-          }}
-        >
-          <Alert severity="success" sx={{ flexGrow: 1 }}>
-            <AlertTitle>Success</AlertTitle>
-            This is a success alert!
-          </Alert>
-          <Alert severity="error" sx={{ flexGrow: 1 }}>
-            <AlertTitle>Error</AlertTitle>
-            This is an error alert!
-          </Alert>
-          <Alert severity="info" sx={{ flexGrow: 1 }}>
-            <AlertTitle>Info</AlertTitle>
-            This is an info alert!
-          </Alert>
-          <Alert severity="warning" sx={{ flexGrow: 1 }}>
-            <AlertTitle>Warning</AlertTitle>
-            This is a warning alert!
-          </Alert>
-        </Box>
-      </CardContent>
+      <Box
+        sx={{
+          marginTop: "16px",
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          gap: "18px",
+        }}
+      >
+        <Alert severity="success" sx={{ flex: "1 1 200px" }}>
+          <AlertTitle>Success</AlertTitle>
+          This is a success alert!
+        </Alert>
+        <Alert severity="error" sx={{ flex: "1 1 200px" }}>
+          <AlertTitle>Error</AlertTitle>
+          This is an error alert!
+        </Alert>
+        <Alert severity="info" sx={{ flex: "1 1 200px" }}>
+          <AlertTitle>Info</AlertTitle>
+          This is an info alert!
+        </Alert>
+        <Alert severity="warning" sx={{ flex: "1 1 200px" }}>
+          <AlertTitle>Warning</AlertTitle>
+          This is a warning alert!
+        </Alert>
+      </Box>
+    </CardContent>
     </Card>
   );
 };
