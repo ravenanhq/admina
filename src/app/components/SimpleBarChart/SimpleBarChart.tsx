ac91715e361a7bdedb5d0015fbbe59d2d33e2963
Karthic <karthic.ganesan@ravenantech.com>
2023-12-02 12:02:38 +0530

    Dashboard chart ui issue fixed

--- src/app/components/SimpleBarChart/SimpleBarChart.tsx
+++ src/app/components/SimpleBarChart/SimpleBarChart.tsx
@@ -1,35 +1,49 @@
 "use client";
-import * as React from 'react';
-import { BarChart } from '@mui/x-charts/BarChart';
-import { Grid, Paper } from '@mui/material';
+import React from 'react';
+import { Chart, CategoryScale, LinearScale, BarElement, Title, Tooltip, ChartOptions } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-const xLabels = [
-  'Page A',
-  'Page B',
-  'Page C',
-  'Page D',
-  'Page E',
-  'Page F',
-  'Page G',
-];
+Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
 
-export default function ResponsiveBarChart() {
+const ResponsiveBarChart = () => {
+
+  const data = {
+    labels: ['Page A',
+    'Page B',
+    'Page C',
+    'Page D',
+    'Page E',
+    'Page F',
+    'Page G'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [4000, 3000, 2000, 2780, 1890, 2390, 3490],
+        backgroundColor: 'rgb(2, 178, 175, 1)',
+      },
+      {
+        label: 'Dataset 2',
+        data: [2400, 1398, 9800, 3908, 4800, 3800, 4300],
+        backgroundColor: 'rgb(46, 150, 255, 1)',
+      },
+    ],
+  };
+
+  const options: ChartOptions<'bar'> = {
+    scales: {
+      x: {
+        stacked: false
+      },
+      y: {
+        stacked: false
+      },
+    },
+  };
+
+
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-
-          <BarChart
-            height={390}
-            series={[
-              { data: pData, label: 'pv', id: 'pvId' },
-              { data: uData, label: 'uv', id: 'uvId' },
-            ]}
-            xAxis={[{ data: xLabels, scaleType: 'band' }]}
-          />
-
-      </Grid>
-    </Grid>
+      <Bar data={data} options={options} />
   );
-}
+};
+
+export default ResponsiveBarChart;
