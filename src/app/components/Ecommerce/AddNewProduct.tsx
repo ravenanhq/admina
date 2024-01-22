287c5ded2c85435681cac12a7232baacdd409872
Raji <raji@ravenantech.com>
2024-01-22 13:03:58 +0530

    [Bug-29]-Fixed validation border color and Changed field order.

--- src/app/components/Ecommerce/AddNewProduct.tsx
+++ src/app/components/Ecommerce/AddNewProduct.tsx
@@ -16,10 +16,16 @@ import {
     Snackbar,
 } from "@mui/material";
 import { useRouter } from "next/navigation";
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const AddNewProduct = () => {
     const [successMessageOpen, setSuccessMessageOpen] = useState(false);
     const router = useRouter();
+
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+    
     const [formData, setFormData] = useState({
         name: "",
         price: "",
@@ -146,7 +152,7 @@ const AddNewProduct = () => {
 
             <CardContent>
                 <Grid container spacing={2}>
-                    <Grid item xs={12} md={6} style={{}}>
+                    <Grid item xs={12} md={6}>
                         <form onSubmit={handleSubmit}>
                             <TextField
                                 fullWidth
@@ -159,7 +165,18 @@ const AddNewProduct = () => {
                                 helperText={errors.title}
                                 size="small"
                             />
-                            <TextField
+                              <TextField
+                                fullWidth
+                                margin="normal"
+                                label="Name"
+                                name="name"
+                                value={formData.name}
+                                onChange={handleChange}
+                                error={!!errors.name}
+                                helperText={errors.name}
+                                size="small"
+                            />
+                             <TextField
                                 fullWidth
                                 margin="normal"
                                 label="Price"
@@ -171,7 +188,23 @@ const AddNewProduct = () => {
                                 helperText={errors.price}
                                 size="small"
                             />
-                            <FormControl sx={{ mt: 2, minWidth: "100%" }} size="small">
+                            <TextField
+                                fullWidth
+                                margin="normal"
+                                label="Compare at Price"
+                                type="text"
+                                name="compareAtPrice"
+                                value={formData.compareAtPrice}
+                                onChange={handleChange}
+                                error={!!errors.compareAtPrice}
+                                helperText={errors.compareAtPrice}
+                                size="small"
+                            />
+                        </form>
+                    </Grid>
+                    <Grid item xs={12} md={6} style={{ paddingTop: isMobile ? '0' : "" }}>
+                        <form onSubmit={handleSubmit}>
+                        <FormControl sx={{ mt: 2, minWidth: "100%" }} size="small">
                                 <InputLabel id="demo-simple-select-error-label">
                                     Vendor Type
                                 </InputLabel>
@@ -192,88 +225,7 @@ const AddNewProduct = () => {
                                     <MenuItem value="Three">Three</MenuItem>
                                 </Select>
                             </FormControl>
-                            <FormControl sx={{ mt: 2, minWidth: "100%" }} size="small">
-                                <InputLabel id="demo-simple-select-error-label">
-                                    Collection
-                                </InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-error-label"
-                                    id="demo-simple-select-error"
-                                    value={formData.collection}
-                                    label="collection"
-                                    name="collection"
-                                    onChange={handleChange}
-                                    renderValue={(value) => `${value}`}
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    <MenuItem value="One">One</MenuItem>
-                                    <MenuItem value="Two">Two</MenuItem>
-                                    <MenuItem value="Three">Three</MenuItem>
-                                </Select>
-                            </FormControl>
-                            <FormControl sx={{ mt: 2, minWidth: "100%" }} size="small">
-                                <input
-                                    type="file"
-                                    name="image"
-                                    onChange={handleChange}
-                                    accept="image/*"
-                                    style={{
-                                        display: "none",
-                                        marginTop: "10px",
-                                        border: "2px solid #007bff",
-                                        borderRadius: "4px",
-                                    }}
-                                    id="imageInput"
-                                />
-                                <label htmlFor="imageInput">
-                                    <Button
-                                        variant="outlined"
-                                        component="span"
-                                        fullWidth
-                                        size="small"
-                                        sx={{
-                                            color: "#888",
-                                            padding: "34px",
-                                            border: "1px solid #c4c4c4",
-                                        }}
-                                    >
-                                        {formData.imageFileName || "Upload Image"}
-                                    </Button>
-                                </label>
-                                <FormHelperText sx={{ color: "#f44336" }}>
-                                    {errors.image}
-                                </FormHelperText>
-                            </FormControl>
-                        </form>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                        <form onSubmit={handleSubmit}>
-                            <TextField
-                                fullWidth
-                                margin="normal"
-                                label="Name"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                error={!!errors.name}
-                                helperText={errors.name}
-                                size="small"
-                            />
-                            <TextField
-                                fullWidth
-                                margin="normal"
-                                label="Compare at Price"
-                                type="text"
-                                name="compareAtPrice"
-                                value={formData.compareAtPrice}
-                                onChange={handleChange}
-                                error={!!errors.compareAtPrice}
-                                helperText={errors.compareAtPrice}
-                                size="small"
-                            />
-                            <FormControl sx={{ mt: 2, minWidth: "100%" }} size="small">
+                            <FormControl sx={{ mt: 3, minWidth: "100%" }} size="small">
                                 <InputLabel id="demo-simple-select-error-label">
                                     Product Type
                                 </InputLabel>
@@ -294,6 +246,28 @@ const AddNewProduct = () => {
                                     <MenuItem value="Three">Three</MenuItem>
                                 </Select>
                             </FormControl>
+                            <FormControl sx={{ mt: 3, minWidth: "100%" }} size="small">
+                                <InputLabel id="demo-simple-select-error-label">
+                                    Collection
+                                </InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-error-label"
+                                    id="demo-simple-select-error"
+                                    value={formData.collection}
+                                    label="collection"
+                                    name="collection"
+                                    onChange={handleChange}
+                                    renderValue={(value) => `${value}`}
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value="One">One</MenuItem>
+                                    <MenuItem value="Two">Two</MenuItem>
+                                    <MenuItem value="Three">Three</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControl sx={{ mt: 1, minWidth: "100%" }} size="small">
                             <TextField
                                 fullWidth
                                 margin="normal"
@@ -303,9 +277,50 @@ const AddNewProduct = () => {
                                 value={formData.productTag}
                                 onChange={handleChange}
                                 size="small"
-                            />
+                            /></FormControl>
                         </form>
                     </Grid>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <form>
+                    <FormControl sx={{ mt: 2, minWidth: isMobile ? "100%" : "99%",
+                             }} 
+                            size="small"
+                            error={Boolean(errors.image)} >
+                                <input
+                                    type="file"
+                                    name="image"
+                                    onChange={handleChange}
+                                    accept="image/*"
+                                    style={{
+                                        display: "none",
+                                        marginTop: "10px",
+                                        border: "2px solid #007bff",
+                                        borderRadius: "4px",
+                                    }}
+                                    id="imageInput"
+                                />
+                                <label htmlFor="imageInput">
+                                    <Button
+                                        variant="outlined"
+                                        component="span"
+                                        fullWidth
+                                        size="small"
+                                        sx={{
+                                            color: `${errors.image ? "#d32f2f" : "#888"}`,
+                                            padding: "34px",
+                                            border: `1px solid ${errors.image ? '#d32f2f' : '#c4c4c4'}`
+                                          
+                                        }}
+                                    >
+                                        {formData.imageFileName || "Upload Image"}
+                                    </Button>
+                                </label>
+                                <FormHelperText sx={{ color: "#d32f2f" }}>
+                                    {errors.image}
+                                </FormHelperText>
+                            </FormControl>
+                    </form>
                 </Grid>
             </CardContent>
             <CardActions style={{ margin: "0px 14px 14px 14px" }}>
