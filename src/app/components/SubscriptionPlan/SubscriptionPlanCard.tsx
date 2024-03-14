cfb38323f5dddc33955ced337413ee50b7395309
Raji <raji@ravenantech.com>
2024-03-14 12:40:37 +0530

    [Task-47]-Fixed issues in subscription page.

--- src/app/components/SubscriptionPlan/SubscriptionPlanCard.tsx
+++ src/app/components/SubscriptionPlan/SubscriptionPlanCard.tsx
@@ -66,7 +66,7 @@ const SubscriptionPlanCard = () => {
                       color: "#000",
                       fontSize: "25px",
                       position: "absolute",
-                      marginTop: isMobile ? "30px" : isTab ? "20px" : "35px",
+                      marginTop: isMobile ? "30px" : isTab ? "13px" : "20px",
                     }}
                   >
                     {plan.price}
@@ -76,7 +76,7 @@ const SubscriptionPlanCard = () => {
                     sx={{
                       backgroundColor: plan.price,
                       color: "#000",
-                      fontSize: isMobile ? "70px" : isTab ? "50px" : "80px",
+                      fontSize: isMobile ? "70px" : isTab ? "40px" : "60px",
                       display: "inline-block",
                       opacity: 0.1,
                     }}
