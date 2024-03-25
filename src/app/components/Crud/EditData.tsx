72ce0cdda0aea67c77d2f4963c0914f241a0671f
Raji <raji@ravenantech.com>
2024-03-25 15:24:04 +0530

    [Task-50]-Changed the crud list , create and edit form pages.

--- src/app/components/Crud/EditData.tsx
+++ src/app/components/Crud/EditData.tsx
@@ -6,18 +6,24 @@ import {
   CardActions,
   TextField,
   Grid,
-  FormControl,
   InputLabel,
-  Select,
   Snackbar,
   Breadcrumbs,
   Link,
   Typography,
+  InputAdornment,
 } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import ButtonComponent from "../BaseComponent/Button";
+import PhoneIcon from "@mui/icons-material/Phone";
+import PersonIcon from "@mui/icons-material/Person";
+import EmailIcon from "@mui/icons-material/Email";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import PersonPinCircleIcon from "@mui/icons-material/PersonPinCircle";
+import { AddLocation } from "@mui/icons-material";
 
 interface RowData {
   name?: string;
@@ -25,6 +31,8 @@ interface RowData {
 
 const EditData = ({ page }: { page: RowData }) => {
   const [successMessageOpen, setSuccessMessageOpen] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [message, setMessage] = useState("");
   const router = useRouter();
 
@@ -35,6 +43,8 @@ const EditData = ({ page }: { page: RowData }) => {
     id: "",
     name: "",
     streetAddress: "",
+    password: "",
+    confirmPassword: "",
     city: "",
     email: "",
     role: null,
@@ -56,6 +66,8 @@ const EditData = ({ page }: { page: RowData }) => {
   const [errors, setErrors] = useState({
     name: "",
     streetAddress: "",
+    password: "",
+    confirmPassword: "",
     city: "",
     email: "",
     role: null,
@@ -94,6 +106,23 @@ const EditData = ({ page }: { page: RowData }) => {
       newErrors.phone = "";
     }
 
+    if (formData.password.trim() === "") {
+      newErrors.password = "Password is required";
+      isValid = false;
+    } else {
+      newErrors.password = "";
+    }
+
+    if (formData.confirmPassword.trim() === "") {
+      newErrors.confirmPassword = "Confirm Password is required";
+      isValid = false;
+    } else if (formData.password !== formData.confirmPassword) {
+      newErrors.confirmPassword = "Passwords do not match";
+      isValid = false;
+    } else {
+      newErrors.confirmPassword = "";
+    }
+
     setErrors(newErrors);
     return isValid;
   };
@@ -121,6 +150,14 @@ const EditData = ({ page }: { page: RowData }) => {
 
   const handleCancel = () => {
     router.push("/crud/list", { scroll: false });
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
   };
 
   return (
@@ -155,69 +192,143 @@ const EditData = ({ page }: { page: RowData }) => {
           message={message}
         />
         <CardContent>
-          <Grid container spacing={2}>
+          <Grid container spacing={4}>
             <Grid item xs={12} md={6}>
               <form onSubmit={handleSubmit}>
+                <Typography
+                  style={{ fontWeight: "bold", marginBottom: "10px" }}
+                >
+                  1.Account Details
+                </Typography>
+                <InputLabel
+                  htmlFor="user-name"
+                  style={{
+                    fontWeight: "bold",
+                    color: "#5d596c",
+                    fontSize: "15px",
+                  }}
+                >
+                  User Name
+                </InputLabel>
                 <TextField
                   fullWidth
                   margin="normal"
-                  label="User Name"
                   name="name"
                   value={formData.name}
                   onChange={handleChange}
                   error={!!errors.name}
                   helperText={errors.name}
                   size="small"
+                  placeholder="User Name"
+                  style={{
+                    margin: "7px 0 20px 0",
+                    width: "94%",
+                    color: "#5d596c",
+                    fontSize: "15px",
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <PersonIcon
+                        style={{ marginRight: "8px", color: "#5d596c" }}
+                      />
+                    ),
+                  }}
                 />
+                <InputLabel
+                  htmlFor="user-name"
+                  style={{
+                    fontWeight: "bold",
+                    color: "#5d596c",
+                    fontSize: "15px",
+                  }}
+                >
+                  Password
+                </InputLabel>
                 <TextField
                   fullWidth
                   margin="normal"
-                  label="Street Address"
-                  name="streetAddress"
-                  value={formData.streetAddress}
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  value={formData.password}
                   onChange={handleChange}
-                  error={!!errors.streetAddress}
-                  helperText={errors.streetAddress}
+                  error={!!errors.password}
+                  helperText={errors.password}
                   size="small"
+                  style={{ margin: "7px 0 20px 0", width: "94%" }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        {showPassword ? (
+                          <VisibilityOffIcon
+                            onClick={togglePasswordVisibility}
+                            sx={{ cursor: "pointer" }}
+                          />
+                        ) : (
+                          <VisibilityIcon
+                            onClick={togglePasswordVisibility}
+                            sx={{ cursor: "pointer" }}
+                          />
+                        )}
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <InputLabel
+                  htmlFor="user-name"
+                  style={{
+                    fontWeight: "bold",
+                    color: "#5d596c",
+                    fontSize: "15px",
+                  }}
+                >
+                  Confirm Password
+                </InputLabel>
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  placeholder="Confirm Password"
+                  type={showPassword ? "text" : "password"}
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  error={!!errors.confirmPassword}
+                  helperText={errors.confirmPassword}
+                  size="small"
+                  style={{ margin: "7px 0 20px 0", width: "94%" }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        {showPassword ? (
+                          <VisibilityOffIcon
+                            onClick={toggleConfirmPasswordVisibility}
+                            sx={{ cursor: "pointer" }}
+                          />
+                        ) : (
+                          <VisibilityIcon
+                            onClick={toggleConfirmPasswordVisibility}
+                            sx={{ cursor: "pointer" }}
+                          />
+                        )}
+                      </InputAdornment>
+                    ),
+                  }}
                 />
 
+                <InputLabel
+                  htmlFor="user-name"
+                  style={{
+                    fontWeight: "bold",
+                    color: "#5d596c",
+                    fontSize: "15px",
+                  }}
+                >
+                  Email
+                </InputLabel>
                 <TextField
                   fullWidth
                   margin="normal"
-                  label="City"
-                  type="text"
-                  name="city"
-                  value={formData.city}
-                  onChange={handleChange}
-                  error={!!errors.city}
-                  helperText={errors.city}
-                  size="small"
-                />
-                <TextField
-                  fullWidth
-                  margin="normal"
-                  label="Pin Code"
-                  type="text"
-                  name="pinCode"
-                  value={formData.pinCode}
-                  onChange={handleChange}
-                  error={!!errors.pinCode}
-                  helperText={errors.pinCode}
-                  size="small"
-                />
-              </form>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              md={6}
-              style={{ paddingTop: isMobile ? "0" : "" }}
-            >
-              <form onSubmit={handleSubmit}>
-                <TextField
-                  fullWidth
-                  margin="normal"
-                  label="Email"
+                  placeholder="Email"
                   type="text"
                   name="email"
                   value={formData.email}
@@ -225,11 +336,138 @@ const EditData = ({ page }: { page: RowData }) => {
                   error={!!errors.email}
                   helperText={errors.email}
                   size="small"
+                  style={{ margin: "7px 0 20px 0", width: "94%" }}
+                  InputProps={{
+                    startAdornment: (
+                      <EmailIcon
+                        style={{ marginRight: "8px", color: "#5d596c" }}
+                      />
+                    ),
+                    sx: { color: "#5d596c", fontSize: "15px" },
+                  }}
                 />
+              </form>
+            </Grid>
+
+            <Grid
+              item
+              xs={12}
+              md={6}
+              style={{ paddingTop: isMobile ? "0" : "" }}
+            >
+              <form onSubmit={handleSubmit}>
+                <Typography
+                  style={{ fontWeight: "bold", marginBottom: "10px" }}
+                >
+                  2.Personal Information
+                </Typography>
+                <InputLabel
+                  htmlFor="user-name"
+                  style={{
+                    fontWeight: "bold",
+                    color: "#5d596c",
+                    fontSize: "15px",
+                  }}
+                >
+                  Street Address
+                </InputLabel>
                 <TextField
                   fullWidth
                   margin="normal"
-                  label="Phone"
+                  placeholder="Street Address"
+                  name="streetAddress"
+                  value={formData.streetAddress}
+                  onChange={handleChange}
+                  error={!!errors.streetAddress}
+                  helperText={errors.streetAddress}
+                  size="small"
+                  style={{ margin: "7px 0 20px 0", width: "94%" }}
+                  InputProps={{
+                    startAdornment: (
+                      <AddLocation
+                        style={{ marginRight: "8px", color: "#5d596c" }}
+                      />
+                    ),
+                    sx: { color: "#5d596c", fontSize: "15px" },
+                  }}
+                />
+                <InputLabel
+                  htmlFor="user-name"
+                  style={{
+                    fontWeight: "bold",
+                    color: "#5d596c",
+                    fontSize: "15px",
+                  }}
+                >
+                  City
+                </InputLabel>
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  placeholder="City"
+                  type="text"
+                  name="city"
+                  value={formData.city}
+                  onChange={handleChange}
+                  error={!!errors.city}
+                  helperText={errors.city}
+                  size="small"
+                  style={{ margin: "7px 0 20px 0", width: "94%" }}
+                  InputProps={{
+                    startAdornment: (
+                      <AddLocation
+                        style={{ marginRight: "8px", color: "#5d596c" }}
+                      />
+                    ),
+                    sx: { color: "#5d596c", fontSize: "15px" },
+                  }}
+                />
+                <InputLabel
+                  htmlFor="user-name"
+                  style={{
+                    fontWeight: "bold",
+                    color: "#5d596c",
+                    fontSize: "15px",
+                  }}
+                >
+                  Pin Code
+                </InputLabel>
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  placeholder="Pin Code"
+                  type="text"
+                  name="pinCode"
+                  value={formData.pinCode}
+                  onChange={handleChange}
+                  error={!!errors.pinCode}
+                  helperText={errors.pinCode}
+                  size="small"
+                  style={{ margin: "7px 0 20px 0", width: "94%" }}
+                  InputProps={{
+                    startAdornment: (
+                      <PersonPinCircleIcon
+                        style={{ marginRight: "8px", color: "#5d596c" }}
+                      />
+                    ),
+                    sx: { color: "#5d596c", fontSize: "15px" },
+                  }}
+                />
+
+                <InputLabel
+                  htmlFor="user-name"
+                  style={{
+                    fontWeight: "bold",
+                    color: "#5d596c",
+                    fontSize: "15px",
+                  }}
+                >
+                  Phone
+                </InputLabel>
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  placeholder="Phone"
                   type="text"
                   name="phone"
                   value={formData.phone}
@@ -237,31 +475,16 @@ const EditData = ({ page }: { page: RowData }) => {
                   error={!!errors.phone}
                   helperText={errors.phone}
                   size="small"
+                  style={{ margin: "7px 0 20px 0", width: "94%" }}
+                  InputProps={{
+                    startAdornment: (
+                      <PhoneIcon
+                        style={{ marginRight: "8px", color: "#5d596c" }}
+                      />
+                    ),
+                    sx: { color: "#5d596c", fontSize: "15px" },
+                  }}
                 />
-
-                <FormControl sx={{ mt: 2, minWidth: "100%" }}>
-                  <InputLabel shrink htmlFor="demo-simple-select-error-label">
-                    Role
-                  </InputLabel>
-                  <Select
-                    native
-                    labelId="demo-simple-select-error-label"
-                    id="demo-simple-select-error"
-                    value={formData.role}
-                    label="Role"
-                    name="role"
-                    placeholder="Role"
-                    onChange={handleChange}
-                    renderValue={(value) => `${value}`}
-                    inputProps={{
-                      id: "select-multiple-native",
-                    }}
-                  >
-                    <option value="Admin">Admin</option>
-                    <option value="User">User</option>
-                    <option value="Moderate">Moderate</option>
-                  </Select>
-                </FormControl>
               </form>
             </Grid>
           </Grid>
