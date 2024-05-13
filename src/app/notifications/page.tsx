b5fe4497aeebbfcc6055c99adcee8847747ef859
Sysadmin <sudharsan@ravenantech.com>
2024-05-13 16:05:07 +0530

    Implement Notification Single page

---
+++ src/app/notifications/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Notifications from "../components/NotificationDetails/Notifications";
+
+const notificationPage = () => {
+  return (
+    <section>
+      <div style={{ paddingTop: 10 }}>
+        <Notifications />
+      </div>
+    </section>
+  );
+};
+export default notificationPage;
