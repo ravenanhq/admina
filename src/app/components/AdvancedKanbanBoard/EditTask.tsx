2ac6563405060459b04eadcf445dacd57f95b202
Raji <raji@ravenantech.com>
2024-08-03 11:41:59 +0530

    [Bug-846]-Removed unused imports.

--- src/app/components/AdvancedKanbanBoard/EditTask.tsx
+++ src/app/components/AdvancedKanbanBoard/EditTask.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css"; // Import Quill styles
+import "react-quill/dist/quill.snow.css"; 
 import {
   Modal,
   TextField,
