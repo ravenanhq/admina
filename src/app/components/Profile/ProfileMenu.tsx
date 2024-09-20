65176881916d4156a7a1bcfc2eeefd4d093a9446
Raji <raji@ravenantech.com>
2024-09-20 18:58:25 +0530

    [Task-975]-Removed the profile and my accocunt page , Notification list.

--- src/app/components/Profile/ProfileMenu.tsx
+++ src/app/components/Profile/ProfileMenu.tsx
@@ -1,15 +1,6 @@
 import React, { useState } from "react";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import Logout from "@mui/icons-material/Logout";
-import { signOut } from "next-auth/react";
-import ManageAccountsOutlinedIcon from "@mui/icons-material/ManageAccountsOutlined";
 import Avatar from "../../../Icons/avatar-header.svg";
 import { Typography } from "@mui/material";
 import ExpandMoreSharpIcon from "@mui/icons-material/ExpandMoreSharp";
@@ -23,18 +14,9 @@ const ProfileMenu = () => {
     setAnchorEl(open ? null : event.currentTarget);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleSignout = async () => {
-    await signOut();
-    handleClose();
-  };
-
   return (
     <React.Fragment>
-      <Tooltip title="Account settings">
+      <Tooltip title="">
         <IconButton
           color="inherit"
           aria-controls={open ? "account-menu" : undefined}
@@ -56,76 +38,6 @@ const ProfileMenu = () => {
           {open ? <ExpandLessIcon /> : <ExpandMoreSharpIcon />}
         </IconButton>
       </Tooltip>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-            mt: 1.5,
-            "& .MuiAvatar-root": {
-              width: 32,
-              height: 32,
-              mr: 1,
-            },
-            "&::before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-      >
-        <MenuItem
-          component="a"
-          href="/profile"
-          onClick={handleClose}
-          sx={{ fontSize: "12px" }}
-        >
-          <ListItemIcon>
-            <PersonOutlinedIcon fontSize="small" />
-          </ListItemIcon>
-          Profile
-        </MenuItem>
-        <MenuItem
-          component="a"
-          href="/accounts"
-          onClick={handleClose}
-          sx={{ fontSize: "12px" }}
-        >
-          <ListItemIcon>
-            <ManageAccountsOutlinedIcon fontSize="small" />
-          </ListItemIcon>
-          My account
-        </MenuItem>
-        <Divider />
-        <MenuItem onClick={handleClose} sx={{ fontSize: "12px" }}>
-          <ListItemIcon>
-            <SettingsOutlinedIcon fontSize="small" />
-          </ListItemIcon>
-          Settings
-        </MenuItem>
-        <MenuItem onClick={handleSignout} sx={{ fontSize: "12px" }}>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
-      </Menu>
     </React.Fragment>
   );
 };
