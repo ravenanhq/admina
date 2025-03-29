33c812e6a3b78a2ab58fc42aa3e06e6c7ec20e26
shanmugapriya <shanmugapriya@ravenantech.com>
2025-03-29 17:00:01 +0530

    [Task-1494]Button ui enhancement done

--- src/app/components/ButtonElement/GroupButton.tsx
+++ src/app/components/ButtonElement/GroupButton.tsx
@@ -1,32 +1,45 @@
 import React from "react";
-import { Box, Button, ButtonGroup, Divider, Typography } from "@mui/material";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardContent,
+  CardHeader,
+  Stack,
+} from "@mui/material";
 
 const GroupButton = () => {
   return (
     <>
-      <Typography
-        variant="h6"
-        gutterBottom
-        sx={{ marginTop: "20px", fontSize: "14px", color: "#565656" }}
-      >
-        Group Buttons
-      </Typography>
-      <Divider sx={{ margin: "0 auto", marginY: 2 }} />
-      <Box sx={{ display: "flex", flexWrap: "wrap", gap: "8px" }}>
-        <ButtonGroup
-          variant="contained"
-          aria-label="outlined primary button group"
-        >
-          <Button>One</Button>
-          <Button>Two</Button>
-          <Button>Three</Button>
-        </ButtonGroup>
-        <ButtonGroup variant="text" aria-label="text success group">
-          <Button>One</Button>
-          <Button>Two</Button>
-          <Button>Three</Button>
-        </ButtonGroup>
-      </Box>
+      <div style={{ border: "1px solid #c0c0c0" }}>
+        <CardHeader
+          title=" Group Button"
+          sx={{ bgcolor: "#1976d2", color: "white" }}
+          titleTypographyProps={{ fontSize: "14px" }}
+        />
+        <Card>
+          <CardContent>
+            <Stack
+              direction="row"
+              spacing={2}
+              sx={{
+                alignItems: "center",
+                justifyContent: "normal",
+                padding: "11px 0",
+                minHeight:"80px",
+              }}
+            >
+              <ButtonGroup
+                variant="outlined"
+                aria-label="text success group"
+              >
+                <Button>Home Button</Button>
+                <Button>About Button</Button>
+              </ButtonGroup>
+            </Stack>
+          </CardContent>
+        </Card>
+      </div>
     </>
   );
 };
